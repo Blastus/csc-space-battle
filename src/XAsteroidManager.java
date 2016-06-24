d1f3735087cee199ba929655d50c2128977601d2
Stephen "Zero" Chappell <Noctis.Skytower@gmail.com>
2016-06-24 08:46:10 -0500

    Creates candidate for EOL 2.

--- src/XAsteroidManager.java
+++ src/XAsteroidManager.java
@@ -15,14 +15,31 @@ class XAsteroidManager {
     private ArrayList<XAsteroid> asteroids;
 
     XAsteroidManager(Dimension size, XPlayer player) {
+        this.size = size;
+        this.player = player;
+        this.asteroids = new ArrayList<>();
     }
 
     void spawn() {
+        XVector asteroidPosition = new XVector();
+        int safetyMargin = ((STARTING_ASTEROID_DIAMETER >> 1) + XPlayer.RADIUS) * SAFETY_SCALE;
+        XVector playerPosition = this.player.getPosition();
+        IntStream.range(0, STARTING_ASTEROID_COUNT).forEach(a -> {
+            do {
+                asteroidPosition.random(this.size);
+            } while (asteroidPosition.sub(playerPosition).getMagnitude() < safetyMargin);
+            this.asteroids.add(new XAsteroid(
+                    new XCircle(asteroidPosition.copy(), STARTING_ASTEROID_DIAMETER),
+                    XVector.polar(STARTING_ASTEROID_SPEED, XSpaceBattle.CHAOS.uniform(XVector.CIRCLE_8_8))
+            ));
+        });
     }
 
     void move() {
+        this.asteroids.forEach(asteroid -> asteroid.move(this.size));
     }
 
     void draw(Graphics surface) {
+        this.asteroids.forEach(asteroid -> asteroid.draw(surface));
     }
 }
