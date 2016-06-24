9b94cf4d3356c2404a8a63b9d320d7f206429c38
Stephen "Zero" Chappell <Blastus@users.noreply.github.com>
2016-06-24 14:04:43 -0500

    Creates candidate for EOL 3.

--- src/XWeapon.java
+++ src/XWeapon.java
@@ -12,15 +12,27 @@ class XWeapon extends XCircle {
     private long bornTime;
 
     XWeapon(XVector position, XVector velocity, long currentTime) {
+        super(position, PROJECTILE_DIAMETER);
+        this.velocity = velocity;
+        this.bornTime = currentTime;
     }
 
     void move(Dimension size) {
+        this.position.ipAdd(this.velocity);
+        this.position.clampXY(size);
     }
 
     void draw(Graphics surface) {
+        surface.setColor(WEAPON_COLOR);
+        surface.fillOval(
+                this.position.getIntX() - this.radius,
+                this.position.getIntY() - this.radius,
+                this.diameter,
+                this.diameter
+        );
     }
 
     boolean isAlive(long currentTime) {
-        return false;
+        return currentTime - this.bornTime <= PROJECTILE_LIFE_SPAN;
     }
 }
