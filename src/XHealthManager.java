bba20c3826a0d71e1ff1cac181715c8471a5812d
Stephen "Zero" Chappell <Noctis.Skytower@gmail.com>
2016-06-27 15:55:54 -0500

    Creates candidate for EOL 4.

--- src/XHealthManager.java
+++ src/XHealthManager.java
@@ -6,21 +6,48 @@ import java.util.stream.IntStream;
  * Created by Stephen "Zero" Chappell on 8 June 2016.
  */
 class XHealthManager {
-    private static int STARTING_LIFE_COUNT;
-    private static int INDICATOR_MARGIN;
-    private static int POINTS_FOR_EXTRA_LIFE;
-    private static Color SHIP_HIGHLIGHT;
-    private static Color SHIP_COLOR;
+    private static int STARTING_LIFE_COUNT = 1;
+    private static int INDICATOR_MARGIN = 5;
+    private static int POINTS_FOR_EXTRA_LIFE = 25;
+    private static Color SHIP_HIGHLIGHT = XPlayer.SHIP_HIGHLIGHT.interpolate(0.5, XColor.BLACK).value();
+    private static Color SHIP_COLOR = XPlayer.SHIP_COLOR.interpolate(0.5, XColor.BLACK).value();
+    private Dimension size;
+    private BufferedImage buffer;
     private int livesRemaining;
     private int totalPoints;
 
     XHealthManager(Dimension size, XPolygon shape) {
+        this.size = size;
+        this.livesRemaining = STARTING_LIFE_COUNT;
+        this.totalPoints = 0;
+        this.buffer = new BufferedImage(
+                shape.getWidthInt() + (INDICATOR_MARGIN << 1),
+                shape.getHeightInt() + (INDICATOR_MARGIN << 1),
+                BufferedImage.TYPE_INT_ARGB
+        );
+        Graphics surface = this.buffer.getGraphics();
+        shape.translate(new XVector(-shape.getMinX(), -shape.getMinY()).add(INDICATOR_MARGIN));
+        shape.draw(surface, SHIP_HIGHLIGHT, SHIP_COLOR);
     }
 
     void addPoints(int points) {
+        int oldEarnedLives = this.totalPoints / POINTS_FOR_EXTRA_LIFE;
+        this.totalPoints += points;
+        int newEarnedLives = this.totalPoints / POINTS_FOR_EXTRA_LIFE;
+        this.livesRemaining += newEarnedLives - oldEarnedLives;
     }
 
     void draw(Graphics surface) {
+        int sizeWidth = (int) this.size.getWidth();
+        int sizeHeight = (int) this.size.getHeight();
+        int bufferWidth = this.buffer.getWidth();
+        int bufferHeight = this.buffer.getHeight();
+        IntStream.range(1, this.livesRemaining + 1).forEach(offset -> surface.drawImage(
+                this.buffer,
+                sizeWidth - bufferWidth * offset,
+                sizeHeight - bufferHeight,
+                null
+        ));
     }
 
     int getScore() {
