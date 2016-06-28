9d677869b96e22ed1e78b64c01a8a5618b5b6188
Stephen "Zero" Chappell <Noctis.Skytower@gmail.com>
2016-06-28 12:31:56 -0500

    Creates candidate for EOL 5.

--- src/XSpecialEffects.java
+++ src/XSpecialEffects.java
@@ -6,8 +6,8 @@ import java.util.stream.IntStream;
  * Created by Stephen "Zero" Chappell on 9 June 2016.
  */
 class XSpecialEffects {
-    private static double MIN_DEBRIS_VELOCITY;
-    private static double MAX_DEBRIS_VELOCITY;
+    private static double MIN_DEBRIS_VELOCITY = 0.5;
+    private static double MAX_DEBRIS_VELOCITY = 1.5;
     private final Dimension size;
     private final ArrayList<XDebris> debris;
     private final ArrayList<XExplosion> explosions;
@@ -19,20 +19,33 @@ class XSpecialEffects {
     }
 
     void spawn(XAsteroid asteroid, long currentTime) {
+        IntStream.range(0, asteroid.getRadius()).forEach(a -> {
+            XVector position = asteroid.getPosition().copy();
+            XVector velocity = XVector.polar(
+                    asteroid.getVelocity().getMagnitude() * XSpaceBattle.CHAOS.uniform(
+                            MIN_DEBRIS_VELOCITY,
+                            MAX_DEBRIS_VELOCITY
+                    ),
+                    XSpaceBattle.CHAOS.uniform(XVector.CIRCLE_8_8)
+            );
+            int diameter = XSpaceBattle.CHAOS.randomInteger(2, 4);
+            this.debris.add(new XDebris(this.size, position, velocity, diameter, currentTime));
+        });
     }
 
     void moveDebris() {
+        this.debris.forEach(XDebris::move);
     }
 
     void drawDebris(Graphics surface, long currentTime) {
+        this.debris.removeIf(particle -> particle.draw(surface, currentTime));
     }
 
     void spawn(XVector position, int effectSize, int particleSize, int particleCount, int lifeSpan, long currentTime) {
-    }
-
-    void moveExplosions(long currentTime) {
+        this.explosions.add(new XExplosion(position, effectSize, particleSize, particleCount, lifeSpan, currentTime));
     }
 
     void drawExplosions(Graphics surface, long currentTime) {
+        this.explosions.removeIf(explosion -> explosion.draw(surface, currentTime));
     }
 }
