9387c8e2db629aab612ec48f2b00c4c9e34ba409
Stephen "Zero" Chappell <Blastus@users.noreply.github.com>
2017-08-09 15:40:06 -0500

    Adjusts XClusterCrack to use a constant for speed deviance.

--- src/XClusterCrack.java
+++ src/XClusterCrack.java
@@ -10,7 +10,8 @@ class XClusterCrack extends XWeapon {
     private static final int PELLET_RADIUS = 2;
     private static final int PELLET_LIFE_SPAN = 700;
     private static final int PELLET_BURST_COUNT = 15;
-    private static final double PELLET_DEVIANCE = XVector.CIRCLE_1_8 / 9;
+    private static final int PELLET_SPEED_DEVIANCE = 1;
+    private static final double PELLET_DIRECTION_DEVIANCE = XVector.CIRCLE_1_8 / 9;
 
     private XClusterCrack(Dimension size, XVector position, XVector velocity, long currentTime) {
         super(size, position, velocity, PELLET_RADIUS, PELLET_LIFE_SPAN, currentTime);
@@ -19,10 +20,11 @@ class XClusterCrack extends XWeapon {
     static ArrayList<XClusterCrack> fire(Dimension size, XPlayer player, long currentTime) {
         ArrayList<XClusterCrack> pellets = new ArrayList<>();
         IntStream.range(0, PELLET_BURST_COUNT).forEach(a -> {
-            double deviance = XRandom.sUniform(-PELLET_DEVIANCE, +PELLET_DEVIANCE);
+            double speedDeviance = XRandom.sUniform(-PELLET_SPEED_DEVIANCE, +PELLET_SPEED_DEVIANCE);
+            double directionDeviance = XRandom.sUniform(-PELLET_DIRECTION_DEVIANCE, +PELLET_DIRECTION_DEVIANCE);
             XVector impulse = XVector.polar(
-                    XWeaponManager.MAX_WEAPON_SPEED + XRandom.sUniform(-1, +1),
-                    player.getDirection() + deviance
+                    XWeaponManager.MAX_WEAPON_SPEED + speedDeviance,
+                    player.getDirection() + directionDeviance
             );
             XVector velocity = player.getVelocity().add(impulse);
             pellets.add(new XClusterCrack(size, player.getPosition().copy(), velocity, currentTime));
